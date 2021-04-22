cbb8873056e816d973f52152b869d1c9bd1d809b
Frederic Meyer <fleroviux@gmail.com>
2021-04-22 23:16:51 +0200

    Basic project setup

---
+++ include/lunatic/integer.hpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2021 fleroviux
+ */
+
+#pragma once
+
+#include <cstdint>
+
+#ifdef LUNATIC_NO_CUSTOM_INT_TYPES
+namespace lunatic {
+#endif
+
+using u8 = std::uint8_t;
+using s8 = std::int8_t;
+using u16 = std::uint16_t;
+using s16 = std::int16_t;
+using u32 = std::uint32_t;
+using s32 = std::int32_t;
+using u64 = std::uint64_t;
+using s64 = std::int64_t;
+
+using uint = unsigned int;
+
+#ifdef LUNATIC_NO_CUSTOM_INT_TYPES
+} // namespace lunatic
+#endif
---
+++ test/main.cpp
@@ -0,0 +1,9 @@
+/*
+ * Copyright (C) 2021 fleroviux
+ */
+
+#include <lunatic/integer.hpp>
+
+int main(int argc, char** argv) {
+  return 0;
+}
