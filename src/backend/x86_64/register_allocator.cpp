ccfffe7539f1e4721212adf1698c46cc748f448a
Frederic Meyer <fleroviux@gmail.com>
2021-05-02 02:07:24 +0200

    Added BSD 3-clause license

--- src/backend/x86_64/register_allocator.cpp
+++ src/backend/x86_64/register_allocator.cpp
@@ -1,5 +1,8 @@
 /*
- * Copyright (C) 2021 fleroviux
+ * Copyright (C) 2021 fleroviux. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
  */
 
 #include "register_allocator.hpp"
