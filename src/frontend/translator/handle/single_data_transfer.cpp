8cf3963d15aea4a439fa21c4293e83ec617c99f7
Frederic Meyer <fleroviux@gmail.com>
2021-05-04 22:27:26 +0200

    Frontend\Translator: start emitting IR for single data transfer opcodes.

--- src/frontend/translator/handle/single_data_transfer.cpp
+++ src/frontend/translator/handle/single_data_transfer.cpp
@@ -11,6 +11,71 @@ namespace lunatic {
 namespace frontend {
 
 auto Translator::Handle(ARMSingleDataTransfer const& opcode) -> bool {
+  if (opcode.condition != Condition::AL) {
+    return false;
+  }
+
+  if (!opcode.pre_increment && opcode.writeback) {
+    // LDRT and STRT are not supported right now.
+    return false;
+  }
+
+  auto offset = IRValue{};
+
+  if (opcode.immediate) {
+    offset = IRConstant{opcode.offset_imm};
+  } else {
+    auto& offset_reg = emitter->CreateVar(IRDataType::UInt32, "base_offset_reg");
+    auto& offset_var = emitter->CreateVar(IRDataType::UInt32, "base_offset_shifted");
+
+    emitter->LoadGPR(IRGuestReg{opcode.offset_reg.reg, mode}, offset_reg);
+
+    // TODO: write a helper method to make this pattern less verbose.
+    // Or alternatively pass the shift type to the emitter directly?
+    switch (opcode.offset_reg.shift) {
+      case Shift::LSL: {
+        emitter->LSL(offset_var, offset_reg, IRConstant{opcode.offset_reg.amount}, false);
+        break;
+      }
+      case Shift::LSR: {
+        emitter->LSR(offset_var, offset_reg, IRConstant{opcode.offset_reg.amount}, false);
+        break;
+      }
+      case Shift::ASR: {
+        emitter->ASR(offset_var, offset_reg, IRConstant{opcode.offset_reg.amount}, false);
+        break;
+      }
+      case Shift::ROR: {
+        emitter->ROR(offset_var, offset_reg, IRConstant{opcode.offset_reg.amount}, false);
+        break;
+      }
+    }
+
+    offset = offset_var;
+  }
+
+  auto& base_old = emitter->CreateVar(IRDataType::UInt32, "base_old");
+  auto& base_new = emitter->CreateVar(IRDataType::UInt32, "base_new");
+
+  emitter->LoadGPR(IRGuestReg{opcode.reg_base, mode}, base_old);
+
+  if (opcode.add) {
+    emitter->ADD(base_new, base_old, offset, false);
+  } else {
+    emitter->SUB(base_new, base_old, offset, false);
+  }
+
+  auto& address = opcode.pre_increment ? base_new : base_old;
+
+  EmitAdvancePC();
+
+  // Do the actual memory access etc...
+
+  // TODO: handle base == dst case (by performing writeback before destination reg write)
+  if (!opcode.pre_increment || opcode.writeback) {
+    emitter->StoreGPR(IRGuestReg{opcode.reg_base, mode}, base_new);
+  }
+
   return false;
 }
 
