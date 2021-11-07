8d0a7e2543691b43805fb758932d4ff34a8b6283
fleroviux <fleroviux@gmail.com>
2021-11-08 00:58:17 +0100

    Frontend\Translator: first attempt to inline next basic block

--- src/frontend/translator/handle/branch_relative.cpp
+++ src/frontend/translator/handle/branch_relative.cpp
@@ -43,6 +43,13 @@ auto Translator::Handle(ARMBranchRelative const& opcode) -> Status {
   }
 
   emitter->StoreGPR(IRGuestReg{GPR::PC, mode}, IRConstant{branch_address});
+
+  // TODO: rework this optimisation to apply to more cases.
+  if (!opcode.exchange && opcode.condition == Condition::AL) {
+    code_address = branch_address - opcode_size * 3;
+    return Status::Continue;
+  }
+
   return Status::BreakBasicBlock;
 }
 
