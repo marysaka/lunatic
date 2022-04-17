f8d3d9aa16acd8f60125ae73002775f0cd4e33d7
StrikerX3 <ivan.rober@gmail.com>
2022-04-17 18:53:00 -0300

    Extract VTune reporting blocks to functions

--- src/backend/x86_64/backend.cpp
+++ src/backend/x86_64/backend.cpp
@@ -16,6 +16,58 @@
 
 #if LUNATIC_USE_VTUNE
 #include <jitprofiling.h>
+
+namespace vtune {
+
+static void ReportCallBlock(u8* codeBegin, const u8* codeEnd) {
+  if (iJIT_IsProfilingActive() == iJIT_SAMPLING_ON) {
+    char methodName[] = "lunatic_x64_callblock";
+    char moduleName[] = "lunatic-JIT";
+
+    iJIT_Method_Load_V2 jmethod = { 0 };
+    jmethod.method_id = iJIT_GetNewMethodID();
+    jmethod.method_name = methodName;
+    jmethod.method_load_address = static_cast<void*>(codeBegin);
+    jmethod.method_size = static_cast<unsigned int>(codeEnd - codeBegin);
+    jmethod.module_name = moduleName;
+    iJIT_NotifyEvent(iJVM_EVENT_TYPE_METHOD_LOAD_FINISHED_V2, static_cast<void*>(&jmethod));
+  }
+}
+
+static void ReportBasicBlock(BasicBlock& basic_block, const u8* codeEnd) {
+  if (iJIT_IsProfilingActive() == iJIT_SAMPLING_ON) {
+    auto& key = basic_block.key;
+
+    auto modeStr = [&]() -> std::string {
+      using lunatic::Mode;
+
+      switch (key.Mode()) {
+      case Mode::User: return "USR";
+      case Mode::FIQ: return "FIQ";
+      case Mode::IRQ: return "IRQ";
+      case Mode::Supervisor: return "SVC";
+      case Mode::Abort: return "ABT";
+      case Mode::Undefined: return "UND";
+      case Mode::System: return "SYS";
+      default: return fmt::format("{:02X}", key.Mode());
+      }
+    }();
+
+    auto thumbStr = key.Thumb() ? "Thumb" : "ARM";
+    auto methodName = fmt::format("lunatic_func_{:X}_{}_{}", key.Address(), modeStr, thumbStr);
+    char moduleName[] = "lunatic-JIT";
+
+    iJIT_Method_Load_V2 jmethod = { 0 };
+    jmethod.method_id = iJIT_GetNewMethodID();
+    jmethod.method_name = methodName.data();
+    jmethod.method_load_address = reinterpret_cast<void*>(basic_block.function);
+    jmethod.method_size = static_cast<unsigned int>(codeEnd - reinterpret_cast<u8*>(basic_block.function));
+    jmethod.module_name = moduleName;
+    iJIT_NotifyEvent(iJVM_EVENT_TYPE_METHOD_LOAD_FINISHED_V2, static_cast<void*>(&jmethod));
+  }
+}
+
+} // namespace vtune
 #endif
 
 /**
@@ -168,18 +220,7 @@ void X64Backend::EmitCallBlock() {
   code->ret();
 
 #if LUNATIC_USE_VTUNE
-  if (iJIT_IsProfilingActive() == iJIT_SAMPLING_ON) {
-    char methodName[] = "lunatic_x64_callblock";
-    char moduleName[] = "lunatic-JIT";
-
-    iJIT_Method_Load_V2 jmethod = { 0 };
-    jmethod.method_id = iJIT_GetNewMethodID();
-    jmethod.method_name = methodName;
-    jmethod.method_load_address = reinterpret_cast<void*>(CallBlock);
-    jmethod.method_size = static_cast<unsigned int>(code->getCurr() - reinterpret_cast<const uint8_t*>(CallBlock));
-    jmethod.module_name = moduleName;
-    iJIT_NotifyEvent(iJVM_EVENT_TYPE_METHOD_LOAD_FINISHED_V2, static_cast<void*>(&jmethod));
-  }
+  vtune::ReportCallBlock(reinterpret_cast<u8*>(CallBlock), code->getCurr());
 #endif
 }
 
@@ -373,34 +414,7 @@ void X64Backend::Compile(BasicBlock& basic_block) {
     }
 
 #if LUNATIC_USE_VTUNE
-    if (iJIT_IsProfilingActive() == iJIT_SAMPLING_ON) {
-      auto& key = basic_block.key;
-
-      auto modeStr = [&]() -> std::string {
-        switch (key.Mode()) {
-        case Mode::User: return "USR";
-        case Mode::FIQ: return "FIQ";
-        case Mode::IRQ: return "IRQ";
-        case Mode::Supervisor: return "SVC";
-        case Mode::Abort: return "ABT";
-        case Mode::Undefined: return "UND";
-        case Mode::System: return "SYS";
-        default: return fmt::format("{:02X}", key.Mode());
-        }
-      }();
-
-      auto thumbStr = key.Thumb() ? "Thumb" : "ARM";
-      auto methodName = fmt::format("lunatic_func_{:X}_{}_{}", key.Address(), modeStr, thumbStr);
-      char moduleName[] = "lunatic-JIT";
-
-      iJIT_Method_Load_V2 jmethod = { 0 };
-      jmethod.method_id = iJIT_GetNewMethodID();
-      jmethod.method_name = methodName.data();
-      jmethod.method_load_address = reinterpret_cast<void*>(basic_block.function);
-      jmethod.method_size = static_cast<unsigned int>(code->getCurr() - reinterpret_cast<uint8_t*>(basic_block.function));
-      jmethod.module_name = moduleName;
-      iJIT_NotifyEvent(iJVM_EVENT_TYPE_METHOD_LOAD_FINISHED_V2, static_cast<void*>(&jmethod));
-    }
+    vtune::ReportBasicBlock(basic_block, code->getCurr());
 #endif
   } catch (Xbyak::Error error) {
     if (int(error) == Xbyak::ERR_CODE_IS_TOO_BIG) {
